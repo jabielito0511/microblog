d6b8fe7d0ff6a09979a6b6a6e670cbb63edc088d
Your Name <your@email.com>
2025-11-24 16:31:38 -0500

    Chapter 16, 20, 21

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,13 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 from config import Config
 
 
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
-    #return 'es'
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -35,6 +36,8 @@ def create_app(config_class=Config):
     mail.init_app(app)
     moment.init_app(app)
     babel.init_app(app, locale_selector=get_locale)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
